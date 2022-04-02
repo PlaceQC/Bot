25d58824a9922c2eefb184dd881b64921072f8f7
PlaceQC <102862848+PlaceQC@users.noreply.github.com>
2022-04-02 13:44:17 -0400

    Updated user script

--- placeqcbot.user.js
+++ placeqcbot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlaceQC Bot
 // @namespace    https://github.com/PlaceQC/Bot
-// @version      2
+// @version      3
 // @description  Le bot pour PlaceQC
 // @author       N_O_P_E (Credit: NoahvdAa)
 // @match        https://www.reddit.com/r/place/*
@@ -15,9 +15,10 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
+// Sorry voor de rommelige code, haast en clean gaatn iet altijd samen ;)
+
 var socket;
 var hasOrders = false;
-var isLoaded = false;
 var accessToken;
 var currentOrderCanvas = document.createElement('canvas');
 var currentOrderCtx = currentOrderCanvas.getContext('2d');
@@ -51,11 +52,12 @@ const COLOR_MAPPINGS = {
     '#9C6926': 25,
     '#000000': 27,
     '#898D90': 29,
+    '#D4D7D9': 30,
+    '#FFFFFF': 31
 };
 
-const pixelCount = canvasWidth * canvasHeight;
 var order = [];
-for (var i = 0; i < pixelCount; i++) {
+for (var i = 0; i < 200000; i++) {
     order.push(i);
 }
 order.sort(() => Math.random() - 0.5);
@@ -66,7 +68,6 @@ order.sort(() => Math.random() - 0.5);
     currentOrderCanvas.height = canvasHeight;
     currentOrderCanvas.style.display = 'none';
     currentOrderCanvas = document.body.appendChild(currentOrderCanvas);
-    
     currentPlaceCanvas.width = canvasWidth;
     currentPlaceCanvas.height = canvasHeight;
     currentPlaceCanvas.style.display = 'none';
@@ -84,29 +85,12 @@ order.sort(() => Math.random() - 0.5);
 
     connectSocket();
     attemptPlace();
+
+    setInterval(() => {
+        if (socket) socket.send(JSON.stringify({ type: 'ping' }));
+    }, 5000);
 })();
-/*
-function setupCanvasSize(canvasWidth, canvasHeight) {
-    currentOrderCanvas.width = canvasWidth;
-    currentOrderCanvas.height = canvasHeight;
 
-    currentPlaceCanvas.width = canvasWidth;
-    currentPlaceCanvas.height = canvasHeight;
-
-    if (!isLoaded) {
-        currentOrderCanvas = document.body.appendChild(currentOrderCanvas);
-        currentPlaceCanvas = document.body.appendChild(currentPlaceCanvas);
-    }
-    const pixelCount = canvasWidth * canvasHeight;
-
-    order = [];
-
-    for (var i = 0; i < pixelCount; i++) {
-        order.push(i);
-    }
-    order.sort(() => Math.random() - 0.5);
-}
-*/
 function connectSocket() {
     Toastify({
         text: 'Connexion au serveur PlaceQC...',
@@ -140,15 +124,6 @@ function connectSocket() {
                 currentOrderCtx = await getCanvasFromUrl(`${serverProtocol}://${serverUrl}/maps/${data.data}`, currentOrderCanvas);
                 hasOrders = true;
                 break;
-            // case 'map_config':
-// 
-            //     Toastify({
-            //         text: `Nouvelle configuration chargée (canvasWidth=${data.canvasWidth}, canvasHeight=${data.canvasHeight})`,
-            //         duration: 10000
-            //     }).showToast();
-// 
-            //     setupCanvasSize(data.canvasWidth, data.canvasHeight);
-            //     isLoaded = true;
             default:
                 break;
         }
@@ -166,75 +141,78 @@ function connectSocket() {
 }
 
 async function attemptPlace() {
-    if (!hasOrders || !isLoaded) {
+    if (!hasOrders) {
         setTimeout(attemptPlace, 2000); // probeer opnieuw in 2sec.
         return;
     }
     var ctx;
     try {
-        const canvasUrl = await getCurrentImageUrl();
-        ctx = await getCanvasFromUrl(canvasUrl, currentPlaceCanvas);
+        ctx = await getCanvasFromUrl(await getCurrentImageUrl('0'), currentPlaceCanvas, 0, 0);
+        ctx = await getCanvasFromUrl(await getCurrentImageUrl('1'), currentPlaceCanvas, 1000, 0)
     } catch (e) {
         console.warn('Erreur lors de la récupération de la carte: ', e);
         Toastify({
-            text: 'Erreur lors de la récupération de la carte. Réessayez dans 15 secondes...',
-            duration: 15000
+            text: 'Erreur lors de la récupération de la carte. Réessayez dans 10 secondes...',
+            duration: 10000
         }).showToast();
-        setTimeout(attemptPlace, 15000); // Réessayez dans 15 secondes
+        setTimeout(attemptPlace, 10000); // Réessayez dans 15 secondes
         return;
     }
 
     const rgbaOrder = currentOrderCtx.getImageData(0, 0, canvasWidth, canvasHeight).data;
     const rgbaCanvas = ctx.getImageData(0, 0, canvasWidth, canvasHeight).data;
 
-    for (const i of order) {
-        // negeer lege order pixels.
-        if (rgbaOrder[(i * 4) + 3] === 0) continue;
+    for (const j of order) {
+        for (var l = 0; l < 10; l++) {
+            const i = (j * 10) + l;
+            // negeer lege order pixels.
+            if (rgbaOrder[(i * 4) + 3] === 0) continue;
 
-        const hex = rgbToHex(rgbaOrder[(i * 4)], rgbaOrder[(i * 4) + 1], rgbaOrder[(i * 4) + 2]);
-        // Deze pixel klopt.
-        if (hex === rgbToHex(rgbaCanvas[(i * 4)], rgbaCanvas[(i * 4) + 1], rgbaCanvas[(i * 4) + 2])) continue;
+            const hex = rgbToHex(rgbaOrder[(i * 4)], rgbaOrder[(i * 4) + 1], rgbaOrder[(i * 4) + 2]);
+            // Deze pixel klopt.
+            if (hex === rgbToHex(rgbaCanvas[(i * 4)], rgbaCanvas[(i * 4) + 1], rgbaCanvas[(i * 4) + 2])) continue;
 
-        const x = i % canvasWidth;
+            const x = i % canvasWidth;
         const y = Math.floor(i / canvasHeight);
-        Toastify({
-            text: `Essai de publier un pixel sur ${x}, ${y}...`,
-            duration: 10000
-        }).showToast();
-
-        const res = await place(x, y, COLOR_MAPPINGS[hex]);
-        const data = await res.json();
-        try {
-            if (data.errors) {
-                const error = data.errors[0];
-                const nextPixel = error.extensions.nextAvailablePixelTs + 3000;
-                const nextPixelDate = new Date(nextPixel);
-                const delay = nextPixelDate.getTime() - Date.now();
-                Toastify({
-                    text: `Pixel posté trop tôt! Le pixel suivant sera placé à ${nextPixelDate.toLocaleTimeString()}.`,
-                    duration: delay
-                }).showToast();
-                setTimeout(attemptPlace, delay);
-            } else {
-                const nextPixel = data.data.act.data[0].data.nextAvailablePixelTimestamp + 3000;
-                const nextPixelDate = new Date(nextPixel);
-                const delay = nextPixelDate.getTime() - Date.now();
-                Toastify({
-                    text: `Pixel placé sur ${x}, ${y}! Le pixel suivant sera placé à ${nextPixelDate.toLocaleTimeString()}.`,
-                    duration: delay
-                }).showToast();
-                setTimeout(attemptPlace, delay);
-            }
-        } catch (e) {
-            console.warn(' Analyser l\'erreur de réponse', e);
             Toastify({
-                text: `Analyser l'erreur de réponse: ${e}.`,
+                text: `Essai de publier un pixel sur ${x}, ${y}...`,
                 duration: 10000
             }).showToast();
-            setTimeout(attemptPlace, 10000);
-        }
 
-        return;
+            const res = await place(x, y, COLOR_MAPPINGS[hex]);
+            const data = await res.json();
+            try {
+                if (data.errors) {
+                    const error = data.errors[0];
+                    const nextPixel = error.extensions.nextAvailablePixelTs + 3000;
+                    const nextPixelDate = new Date(nextPixel);
+                    const delay = nextPixelDate.getTime() - Date.now();
+                    Toastify({
+                        text: `Pixel posté trop tôt! Le pixel suivant sera placé à ${nextPixelDate.toLocaleTimeString()}.`,
+                        duration: delay
+                    }).showToast();
+                    setTimeout(attemptPlace, delay);
+                } else {
+                    const nextPixel = data.data.act.data[0].data.nextAvailablePixelTimestamp + 3000;
+                    const nextPixelDate = new Date(nextPixel);
+                    const delay = nextPixelDate.getTime() - Date.now();
+                    Toastify({
+                        text: `Pixel placé sur ${x}, ${y}! Le pixel suivant sera placé à ${nextPixelDate.toLocaleTimeString()}.`,
+                        duration: delay
+                    }).showToast();
+                    setTimeout(attemptPlace, delay);
+                }
+            } catch (e) {
+                console.warn(' Analyser l\'erreur de réponse', e);
+                Toastify({
+                    text: `Analyser l'erreur de réponse: ${e}.`,
+                    duration: 10000
+                }).showToast();
+                setTimeout(attemptPlace, 10000);
+            }
+
+            return;
+        }
     }
 
     Toastify({
@@ -245,6 +223,7 @@ async function attemptPlace() {
 }
 
 function place(x, y, color) {
+    socket.send(JSON.stringify({ type: 'placepixel', x, y, color }));
     return fetch('https://gql-realtime-2.reddit.com/query', {
         method: 'POST',
         body: JSON.stringify({
@@ -284,7 +263,7 @@ async function getAccessToken() {
     return responseText.split('\"accessToken\":\"')[1].split('"')[0];
 }
 
-async function getCurrentImageUrl() {
+async function getCurrentImageUrl(id = '0') {
     return new Promise((resolve, reject) => {
         const ws = new WebSocket('wss://gql-realtime-2.reddit.com/query', 'graphql-ws');
 
@@ -304,7 +283,7 @@ async function getCurrentImageUrl() {
                             'channel': {
                                 'teamOwner': 'AFD2022',
                                 'category': 'CANVAS',
-                                'tag': '0'
+                                'tag': id
                             }
                         }
                     },
@@ -324,20 +303,19 @@ async function getCurrentImageUrl() {
 
             ws.close();
             resolve(parsed.payload.data.subscribe.data.name + `?noCache=${Date.now() * Math.random()}`);
-
         }
 
         ws.onerror = reject;
     });
 }
 
-function getCanvasFromUrl(url, canvas) {
+function getCanvasFromUrl(url, canvas, x = 0, y = 0) {
     return new Promise((resolve, reject) => {
         var ctx = canvas.getContext('2d');
         var img = new Image();
         img.crossOrigin = 'anonymous';
         img.onload = () => {
-            ctx.drawImage(img, 0, 0);
+            ctx.drawImage(img, x, y);
             resolve(ctx);
         };
         img.onerror = reject;
